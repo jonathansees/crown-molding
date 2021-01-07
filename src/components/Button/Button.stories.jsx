af72e7e0b8655a3933c5488fbda2ce92adaa4ccd
Jonathan Sees <jonathan.m.sees@gmail.com>
2021-01-07 00:47:53 -0800

    fix: added theming to Button component

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { ThemeProvider } from 'styled-components';
+import { withKnobs, text, select } from '@storybook/addon-knobs';
 import { jsxDecorator } from 'storybook-addon-jsx';
 import { withA11y } from '@storybook/addon-a11y';
 import centered from '@storybook/addon-centered/react';
+import themes from '../../utils/themes/utils/keys';
+import config from '../../config';
 import Button from './Button';
 
 export default {
@@ -17,12 +20,86 @@ export default {
 };
 
 export const Default = () => (
-  <>
-    <Button>
-      {text('Label', 'Submit')}
-    </Button>
-    <Button type="transparent">
-      {text('Label', 'Submit')}
-    </Button>
-  </>
+  <ThemeProvider theme={{ mode: select('Theme', themes, config.theme.mode) }}>
+    <div>
+      <Button size="large">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button size="medium">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button size="small">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button invert size="large">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button invert size="medium">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button invert size="small">
+        {text('Label', 'Submit')}
+      </Button>
+    </div>
+    <div>
+      <Button variant="passive" size="large">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button variant="passive" size="medium">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button variant="passive" size="small">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button invert variant="passive" size="large">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button invert variant="passive" size="medium">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button invert variant="passive" size="small">
+        {text('Label', 'Submit')}
+      </Button>
+    </div>
+    <div>
+      <Button variant="special" size="large">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button variant="special" size="medium">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button variant="special" size="small">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button invert variant="special" size="large">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button invert variant="special" size="medium">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button invert variant="special" size="small">
+        {text('Label', 'Submit')}
+      </Button>
+    </div>
+    <div>
+      <Button variant="destructive" size="large">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button variant="destructive" size="medium">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button variant="destructive" size="small">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button invert variant="destructive" size="large">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button invert variant="destructive" size="medium">
+        {text('Label', 'Submit')}
+      </Button>
+      <Button invert variant="destructive" size="small">
+        {text('Label', 'Submit')}
+      </Button>
+    </div>
+  </ThemeProvider>
 );
--- src/components/Button/Button.styled.jsx
+++ src/components/Button/Button.styled.jsx
@@ -1,53 +1,93 @@
 import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
-import config from '../../config';
-import Themes from '../../utils/themes';
-import { secondaryColor, bgColor } from '../../utils/colors';
-
-const colorProperties = (background, color, border) => css`
-  background: ${background};
-  color: ${color};
-  border: 2px solid ${border};
-`;
-
-const handleTypeColor = (type) => {
-  switch (type) {
-    case 'inverse':
-      return colorProperties(bgColor, secondaryColor, secondaryColor);
-    case 'transparent':
-      return colorProperties('transparent', bgColor, bgColor);
-    default:
-      return colorProperties(secondaryColor, bgColor, secondaryColor);
-  }
-};
+import { Spinner } from '@styled-icons/fa-solid/Spinner';
+import {
+  textColor,
+  backgroundColor,
+  hoverBackgroundColor,
+  activeBackgroundColor,
+  invertHoverBackgroundColor,
+  invertActiveBackgroundColor,
+  padding,
+  sidePadding,
+  minHeight,
+  minWidth,
+  fontSize,
+  fontHeight,
+} from './themes';
 
 const StyledButton = styled.button`
-  border-radius: 8px;
-  display: inline-block;
-  padding: 5px 20px;
-  margin: 10px;
-  text-decoration: none;
-  font-family: sans-serif;
-  font-size: 1rem;
-  line-height: 1;
+  --border-width: 2px;
+  --background-color: ${backgroundColor};
+  --hover-background-color: ${hoverBackgroundColor};
+  --active-background-color: ${activeBackgroundColor};
+  --invert-hover-background-color: ${invertHoverBackgroundColor};
+  --invert-active-background-color: ${invertActiveBackgroundColor};
+  
+  --text-color: ${textColor};
+  --font-size: ${fontSize};
+  --font-height: ${fontHeight};
+
+  background-color: var(--background-color);
+  color: var(--text-color);
   cursor: pointer;
-  text-align: center;
-  transition: background 250ms ease-in-out, transform 150ms ease;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  ${({ type }) => handleTypeColor(type)};
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0;
+  border: var(--border-width) solid var(--background-color);
+  outline: none;
+  box-sizing: border-box;
+  border-radius: 4px;
+  /* Gets rid of the blue highlight on mobile android */
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+
+  font-size: var(--font-size);
+  font-height: var(--font-height);
+
+  ${(props) => props.disabled && css`
+    opacity: 0.4;
+    cursor: not-allowed;
+    pointer-events: none;
+  `}
+
+  &:hover:not(.disabled) {
+    background-color: var(--hover-background-color);
+  }
+
+  &:active {
+    background-color: var(--active-background-color);
+  }
+
+  ${(props) => props.invert && css`
+    background-color: transparent;
+    color: var(--background-color);
+    border: var(--border-width) solid var(--background-color);
+
+    &:hover:not(.disabled) {
+      background-color: var(--background-color);
+      color: var(--text-color);
+    }
+  `}
+
+  padding: ${padding} ${sidePadding};
+  min-height: ${minHeight};
+  min-width: ${minWidth};
+`;
+
+export const StyledSpinner = styled(Spinner)`
+  margin-right: 4px;
+  height: var(--font-size);
 `;
 
 StyledButton.propTypes = {
-  type: PropTypes.oneOf(['inverse', 'transparent']),
-  theme: PropTypes.shape({
-    mode: PropTypes.oneOf(Themes),
-  }),
+  disabled: PropTypes.bool,
+  invert: PropTypes.bool,
 };
 
 StyledButton.defaultProps = {
-  type: undefined,
-  theme: config.defaultTheme,
+  disabled: false,
+  invert: false,
 };
 
 export default StyledButton;
