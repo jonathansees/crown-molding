03a24fc197ff3c6187aa90ea8391a851a23cdc24
Jonathan Sees <jonathan.m.sees@gmail.com>
2021-01-07 01:36:19 -0800

    fix: added additional components to be exported from src/index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import Button from './components/Button';
-import Checkbox from './components/Checkbox';
-import Radio from './components/Radio';
-import Select from './components/Select';
-import Table from './components/Table';
+import Checkbox, { CheckboxItem } from './components/Checkbox';
+import Radio, { RadioItem } from './components/Radio';
+import Select, { Option } from './components/Select';
+import Table, { TableRow, TableHeader, TableColumn } from './components/Table';
 import TextInput from './components/TextInput';
 
 export {
   Button,
   Checkbox,
+  CheckboxItem,
   Radio,
+  RadioItem,
   Select,
+  Option,
   Table,
+  TableRow,
+  TableHeader,
+  TableColumn,
   TextInput,
 };
